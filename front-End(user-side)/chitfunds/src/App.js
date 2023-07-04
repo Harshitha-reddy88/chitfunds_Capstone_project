0b48f24a86632b31bdd18c02f4d414773f48f637
A.Harshitha <athmakuruharshitha@gmail.com>
2023-07-04 09:11:37 +0530

    functionality

--- front-End(user-side)/chitfunds/src/App.js
+++ front-End(user-side)/chitfunds/src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
-//import HomePage from './Components/homepage';
-import BankDetails from './Components/bankdetails';
+import HomePage from './Components/homepage';
+//import BankDetails from './Components/bankdetails';
 function App() {
   return (
     <div className="App">
-      {/* <HomePage />  */}
-      < BankDetails />
+      <HomePage /> 
+      {/* < BankDetails /> */}
     </div>
   );
 }
-
 export default App;
 
 
--- front-End(user-side)/chitfunds/src/Components/homepage.js
+++ front-End(user-side)/chitfunds/src/Components/homepage.js
@@ -11,7 +11,7 @@ function HomePage(){
                             <img  className="logo" src="../resorses/logo.avif" alt="error"/>
                         </div>
                         <div className='arazChiti chiti'>
-                            <h4>Araz Chiti</h4>
+                            <h4>Auction Chiti</h4>
                         </div>
                         <div className='doubleChiti chiti'>
                             <h4>Double chiti</h4>
@@ -56,7 +56,41 @@ function HomePage(){
             </div>
 
             <div>
-                <h2>process</h2>
+                <div></div>
+                <div>
+                    <img/>
+                </div>
+            </div>
+
+            <div>
+                <h2 className='aboutChit'>chit funds provide individuals with a unique financial tool to save, <br/>
+                    invest, borrow, and achieve their financial goals.</h2>
+            </div>
+
+            <div>
+                <div>
+                    <img src="https://thumbs.dreamstime.com/b/selective-focus-auctioneer-holding-gavel-microphone-looking-buyers-auction-175030056.jpg"  alt="error"/>
+                    <h5>Auction chiti, also known as auction-based chit funds, is a<br/>
+                         variant of traditional chit funds that involves the process of <br/>
+                         conducting auctions to determine the recipient of the prize amount. <br/>
+                         These chit funds add an element of excitement and competition to the<br/> 
+                         chit fund experience, providing participantswith an opportunity to <br/>
+                         potentially receive a larger payout.</h5>
+                </div>
+
+                <div>
+                    <img src="https://blog.ipleaders.in/wp-content/uploads/2011/07/How-can-a-Chit-Fund-help-you-to-raise-capital-for-your-business.png" alt="error"/>
+                    <h5>Double chiti, also known as a double-scheme chit fund, is a unique <br/>
+                        variant of traditional chit funds that offers participants the opportunity<br/>
+                        to receive a larger payout than their regular contributions.This type of <br/>
+                        chit fund is structured in a way that allows participants to bid for a <br/>
+                        higherprize amount, increasing the potential returns on their investment.</h5>
+                </div>
+
+                <div>
+                    <img src="" alt="error"/>
+                    <h5></h5>
+                </div>
             </div>
 
             <div className='bankDiv'>
@@ -98,7 +132,8 @@ function HomePage(){
                 <div className='peopleDiv'>
                     <img className="peopleMoney"src="https://previews.123rf.com/images/dolgachov/dolgachov1901/dolgachov190101437/115494320-finance-wealth-and-people-concept-group-of-happy-friends-picking-money-falling-from-up-above-over.jpg" alt="error"/>
 
-                    <h4 className='peopleText'> Witnessing the joy and satisfaction that<br/>
+                    <h4 className='peopleText'> 
+                         Witnessing the joy and satisfaction that<br/>
                          our members experience when it comes to <br/>
                          their finances.The thrill of winning the <br/>
                          joy of achieving your goals with ChitFund <br/>
